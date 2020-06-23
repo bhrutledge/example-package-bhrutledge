9a979f9e659c548961e9ec3a3b74bbfdb8f06e05
Brian Rutledge <brian@bhrutledge.com>
2020-06-23 06:34:11 -0400

    Update .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="example-pkg-bhrutledge",
-    version="0.0.4.dev1",
+    version="0.0.4a4",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
